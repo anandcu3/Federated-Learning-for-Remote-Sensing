7c1c8f66708e7da217bfe637ec0c2d88210a464f
Anand C U <anandcu3@gmail.com>
2021-01-26 16:06:34 +0200

    Added argparse to make it easier to give configurations for the runs

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ The repository consists of the code for Federated Learning Experiments for Remot
 - `CustomDataSet.py` : Inherits the abstract class `torch.utils.data.Dataset` and overrides `__len__` and `__getitem__` method. This custom dataset class supports Multilabel for each image.
 - `federated_train_algorithms.py` : Has the federated training algotihms implemented in it. Supports FedAvg Currently.
 - `FL_with_pytorch_only.ipynb` : The final notebook version from which the modular code was written from.
--  `main.py` : The file to run to start training based on the required experiment setup.
+-  `main.py` : The file to run to start training based on the required experiment setup. Required Arguments are data path and the CNN model to use. For more details on how to use the arguments use `python main.py -h ` and use `python main.py -c resnet34 -d ./UCMerced_LandUse/Images` for example to use resnet34 and with the images in the provided path.
 -  `train.py` : The file has the training loop. This training loop will be common for different federated algorithms.
--- main.py
+++ main.py
@@ -7,17 +7,35 @@ import numpy as np
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 import torch
 
-df = pd.read_excel(Path('multilabels/LandUse_Multilabeled.xlsx').resolve())
+parser = argparse.ArgumentParser(
+    description='Run speech recognition on Google or Azure. Followed by WER for the generated hypothesis.')
+parser.add_argument('--cnn_model', '-c', type=str, required=True,
+                    help='Specify which CNN to use. "lenet" or "resnet34"')
+parser.add_argument('--data_dir', '-d', type=str, required=True,
+                    help='Specify path to images folder of UCMerced_LandUse dataset. Eg. ./UCMerced_LandUse/Images')
+parser.add_argument('--multilabel_excelfilepath', type=str, default='multilabels/LandUse_Multilabeled.xlsx',
+                    help='Specify path to images folder of UCMerced_LandUse dataset. Eg. ./UCMerced_LandUse/Images')
+args = parser.parse_args()
+
+df = pd.read_excel(Path(args.multilabel_excelfilepath).resolve())
 df_label = np.array(df)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class_names = np.array(["airplane", "bare-soil", "buildings", "cars", "chaparral", "court", "dock",
                         "field", "grass", "mobile-home", "pavement", "sand", "sea", "ship", "tanks", "trees", "water"])
-data_dir = Path('./UCMerced_LandUse/Images').resolve()
+data_dir = Path(args.data_dir).resolve()
 trainloaders, valloader = load_split_train_test(data_dir, df_label, .2)
-model = LENET(len(class_names))
-model = RESNET34(len(class_names))
+if args.cnn_model == "lenet":
+    print("Using Lenet")
+    model = LENET(len(class_names))
+elif args.cnn_model == "resnet34":
+    print("Using Resnet 34")
+    model = RESNET34(len(class_names))
+else:
+    print("Unknown CNN")
+    exit()
 
 model = model.to(device)
 criterion = nn.BCEWithLogitsLoss()
